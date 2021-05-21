a7ff813a30d914f12ec5569d3729f291af0706a7
Michał Kwarciński <01158156@pw.edu.pl>
2021-05-21 18:58:26 +0000

    Add new file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "../class/client.h"
+#include "../class/Lifeguard.h"
+#include "../class/Instructor.h"
+#include "../class/atractions.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    Client c1;
+    Atraction a1;
+    Lifeguard l1;
+    Instructor i1;
+}
