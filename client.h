205604f585e397182deb0f5d8edff9140a3a4dd4
Michał Kwarciński <01158156@pw.edu.pl>
2021-05-26 18:37:22 +0000

    Update client.h

--- client.h
+++ client.h
@@ -1,4 +1,4 @@
-﻿#ifndef client_h
+#ifndef client_h
 #define client_h
 
 #include <iostream>
@@ -14,70 +14,47 @@
 class Client
 {
 public:
-    Client()
-    {
-        name = "Unknown";
-        surname = "Unknown";
-        card_id = 0;
-        carnet_id = 0;
-        attractions = {};
-        // bool ulga = false;
-    }
+    Client();
 
-    Client(std::string name,
+    Client(
+        std::string name,
         std::string surname,
-        int card_id,
         int carnet_id,
-        std::vector <std::string> attractions);
+        bool discount,
+        int remaining_time
+        );
 
     Client(const Client& client)
     {
         name = client.name;
         surname = client.surname;
-        card_id = client.card_id;
         carnet_id = client.carnet_id;
-        attractions = client.attractions;
+        discount = client.discount;
+        remaining_time = client.remaining_time;
     }
 
     friend std::ostream& operator << (std::ostream& output, const Client& c);
 
     bool operator == (const Client& c1);
 
-    friend bool operator != (const Client& c1, const Client& c2);
+    bool operator != (const Client& c1);
 
     Client& operator=(const Client& s);
-
-    void add_attraction(std::string title);
-
-    void del_attraction(std::string title);
-
-    void change_name(std::string new_name);
-
-    void change_surname(std::string new_surname);
-
-    void change_carnet_id(int new_carnet_id);
-
-    void change_card_id(int new_card_id);
-
 
     std::string get_name() const;
 
     std::string get_surname() const;
 
-    int get_card_id() const;
-
     int get_carnet_id() const;
-
-    void get_attractions() const;
-
-    std::vector <std::string> attractions;
 
     std::string name;
 
     std::string surname;
 
-    int card_id;
-
     int carnet_id;
+
+    bool discount;
+
+    int remaining_time;
 };
 #endif
