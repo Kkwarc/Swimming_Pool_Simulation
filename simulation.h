aca03fb0e8cda024842675d22179f562456e5393
Michał Kwarciński <01158156@pw.edu.pl>
2021-05-28 16:54:51 +0000

    Update simulation.h

--- simulation.h
+++ simulation.h
@@ -1,20 +1,18 @@
 #ifndef simualtion_h
 #define simualtion_h
 
-#include "client.h"
 #include "da_Pool.h"
-#include "atractions.h"
-#include "Lifeguard.h"
-#include "Instructor.h"
-#include <vector>
-#include <iostream>
 #include <sstream>
+#include <random>
 
 class Simulation {
 public:
+    int max_number_of_enters_per_tick;
+    int tick_length;
+    int par; // parametr losowy
     std::vector < Client > list_of_clients;
     std::vector < Track > list_of_tracks;
-    // std::vector < Atraction > list_of_atractions;
+    std::vector < Atraction > list_of_atractions;
     std::vector < Lifeguard > list_of_lifeguards;
     std::vector < Instructor > list_of_instructors;
     int begginer_tr; // ilosc dla beginerow
@@ -28,9 +26,22 @@ public:
     int n_of_l; // ilosc ratownikow
     int length; // dlugosc toru
     int tr_limit; // ilosc ludzi na tor
+    Da_Pool gowno;
     Time start_time; // godzina otwarcia
     Time closing_time; // godzina zamkniecia
-    void create_pool(); // inicjator poola
+
+    Simulation(
+        std::string databaze
+    );
+
+    //void create_pool(); // inicjator poola
+    void customers_movements(int par);
+    void lifeguards_enters(int par); //  na starcie && zmiany kadry
+    void client_enters();
+    void summary_of_tick();
     void summary_of_day(); // podsumowanie dnia
+    void main_simulation(); // funkcja ktora zawiera wszytskie inne funkcje
+    int number_of_enters();
+    int give_random_number(int zakres);
 };
 #endif
