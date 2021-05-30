a3d8e8af401755d8a5e371c600ef42dc6903a13e
Michał Kwarciński <01158156@pw.edu.pl>
2021-05-30 16:15:22 +0000

    Update da_Pool.cpp

--- da_Pool.cpp
+++ da_Pool.cpp
@@ -113,9 +113,9 @@ void Da_Pool::exit_da_pool(Client& client)
         }
     }
     int index;
-    for (long long unsigned int i=0; i < clients.size(); i++)
+    for (long long unsigned int i = 0; i < clients.size(); i++)
     {
-        if(clients[i].carnet_id == id)
+        if (clients[i].carnet_id == id)
         {
             index = i;
         }
@@ -174,50 +174,50 @@ void Da_Pool::reservation(int tr_nr, Time start, int duration, Instructor& inst,
 
 bool Da_Pool::the_time_is_passing(int tick)
 {
-    if(current_time == closing_time)
+    if (current_time == closing_time)
     {
         return false;
     }
     else
     {
-        for(long long unsigned int i=0; i<clients.size(); i++)
+        for (long long unsigned int i = 0; i < clients.size(); i++)
         {
             if (clients[i].remaining_time == 0)
             {
                 exit_da_pool(clients[i]);
-                i=i-1;
+                i = i - 1;
             }
             clients[i].set_time(clients[i].remaining_time - tick);
         }
-        for(long long unsigned int i=0; i<staff_available.size(); i++)
+        for (long long unsigned int i = 0; i < staff_available.size(); i++)
         {
-            if(staff_available[i].finish == current_time)
+            if (staff_available[i].finish == current_time)
             {
                 staff_exit(staff_available[i]);
-                i=i-1;
+                i = i - 1;
             }
         }
-        for(long long unsigned int i=0; i<staff_available.size(); i++)
+        for (long long unsigned int i = 0; i < staff_available.size(); i++)
         {
-            if(staff_available[i].finish == current_time)
+            if (staff_available[i].finish == current_time)
             {
                 staff_exit(staff_available[i]);
-                i=i-1;
+                i = i - 1;
             }
         }
-        for(long long unsigned int i=0; i<atractions.size(); i++)
+        for (long long unsigned int i = 0; i < atractions.size(); i++)
         {
-            if(atractions[i].name == "Swimming_Pool")
+            if (atractions[i].name == "Swimming_Pool")
             {
                 Swimming_Pool* t = static_cast<Swimming_Pool*>(&atractions[i]);
-                for(long long unsigned int j = 0; i<t->tracks.size();j++)
+                for (long long unsigned int j = 0; i < t->tracks.size(); j++)
                 {
-                    if(t->tracks[j].reservation_time == 0)
+                    if (t->tracks[j].reservation_time == 0)
                     {
                         t->tracks[j].reserved = false;
                         Instructor f;
                         t->tracks[j].instructor = f;
-                        for(long long unsigned int k=0; k<t->tracks[j].people.size(); k++)
+                        for (long long unsigned int k = 0; k < t->tracks[j].people.size(); k++)
                         {
                             t->tracks[j].people[k].did_reserve = false;
                         }
