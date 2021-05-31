8c69412975f786af91544f76eed4a5c5ffb29d20
Michał Kwarciński <01158156@pw.edu.pl>
2021-05-31 17:13:18 +0000

    Update simulation.cpp

--- simulation.cpp
+++ simulation.cpp
@@ -53,7 +53,7 @@ using namespace std;
  	{
  		int rand1 = give_random_number(list_of_clients.size());
  		int rand2 = give_random_number(list_of_atractions.size());
- 		gowno.add_client(list_of_clients[rand1],rand2, 1);
+ 		gowno.add_client(list_of_clients[rand1],rand2, 1, 60);
         list_of_clients[rand1].curent_atr_nr = rand2;
  	}
  }
@@ -63,21 +63,38 @@ using namespace std;
  	int y = n_of_a;
  	for (int i = 0; i < y; i++)
  	{
- 		// prin atraction -> lifeguards -> clients
- 		// numer atrakcji -> dane ratownika(imie, nazawisko) -> ilosc klientow
- 		// ...
+        // do dopisania operator strumienia w Time
+        cout << "Time: " << gowno.current_time << " " << (gowno.current_time + tick_length) << endl;
+        cout << "Artaction number: " << list_of_atractions[i].atraction_nr <<
+            " Lifeguards data [name and surname: " << list_of_atractions[i].lifeguard.name << 
+             "/" << list_of_atractions[i].lifeguard.surname <<
+            " Number of clients: " << list_of_atractions[i].people.size() << endl;
  	}
  }
 
  void Simulation::summary_of_day()
  {
- 	// print -> godzina -> odpowiednia lista z ticku
- 	// Bartek naaaaaaaaaaaaapisz
+    // cos do zapisywania -> wektor strukow (jak time) -> operator wypisywania
+    // trzeba zapisywac te dane przy wejsciu clienta 
+    // godzina wyjsca = curent time + 60
+
+    // dane klienta
+    // godzina przyjsca
+    // godzina wyjscia
+    // ile zapłacił
  }
 
  void Simulation::main_simulation()
  {
-
+     // pobranie i zainiciowanie obiektów z databaze
+     // rorpoczecie pętli
+     // -> rozklad ratownikow na basenie w godzinie startowej i czasie zmiany (u nas 14)
+     // -> przejscia klientow
+     // -> wchodzenie klientow
+     // -> summary of tick
+     // -> thetimeispassing
+     // koniec petli
+     // -> summary of day
  }
 
   int Simulation::number_of_enters()
