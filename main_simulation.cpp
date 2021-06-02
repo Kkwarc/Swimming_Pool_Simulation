58ac533f09da07e0d25ca27b044cee9bf7f0af0c
Michał Kwarciński <01158156@pw.edu.pl>
2021-06-02 18:29:52 +0000

    Update main_simulation.cpp

--- main_simulation.cpp
+++ main_simulation.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include "simulation.h"
-
-// klienci siedza na basenie tyle ile wczesniej wykupili lub mniej
-// cykl 10 minutowy
+// klienci siedza na basenie tyle ile wczesniej wykupi lub mniej
+// cykl 5 minutowy
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    Client c1;
-    Atraction a1;
-    Lifeguard l1;
-    Instructor i1;
+    Simulation s("Databaze");
+    s.main_simulation();
 }
+
