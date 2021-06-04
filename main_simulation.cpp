943df33ada1723cb8f4950c7b6ca6ad497f50e83
Michał Kwarciński <01158156@pw.edu.pl>
2021-06-04 21:01:52 +0000

    Update main_simulation.cpp

--- main_simulation.cpp
+++ main_simulation.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include "simulation.h"
+#include <string>
+#include <fstream>
+
+using namespace std;
+
 // klienci siedza na basenie tyle ile wczesniej wykupi lub mniej
-// cykl 5 minutowy
+// cykl 30 minutowy
+// mamy najlepszych ratownikow w polsce -> jeden na atrakcje
 
 int main()
 {
-    Simulation s("databaze.txt");
+    ofstream ofs1;
+    ofs1.open("logi.txt", ofstream::out | ofstream::trunc);
+    ofs1.close();
+    ofstream ofs2;
+    ofs2.open("podsumowania.txt", ofstream::out | ofstream::trunc);
+    ofs2.close();
+    Simulation s("../class/Databaze.txt");
     s.main_simulation();
 }
-
